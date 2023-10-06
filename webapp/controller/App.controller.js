ea46cafad303d61afd1abf28bd8ca95e61705311
Siolto <simon.coen@js-soft.com>
2023-10-06 13:03:58 +0200

    chore: fpm-ready-app

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/fe/core/PageController",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox",
+	"sap/ui/model/Sorter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/FilterType",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, MessageBox, Sorter, Filter, FilterOperator, FilterType,
+	JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {});
+});
